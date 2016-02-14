30f0ad4ad96a8b98d9036f172d17f3b4f56242fc
alaboudi1 <alobodi1@hotmail.com>
2016-02-14 21:39:47 +0000

    Gutter V0.6

---
+++ include/stub.js
@@ -0,0 +1,24 @@
+
+
+   // this a stub "fake" code is used to simulate the sinario in which other JS code calls the gutter API
+   
+   function fake(){
+       
+       
+           var c  = ['1','2'];
+           setContentGutter(1, c);  
+            var c  = ['1','3'];
+           setContentGutter(2, c);
+           var c  = ['1','2'];
+           setContentGutter(1, c);  
+            var c  = ['1','3'];
+           setContentGutter(2, c);
+           var c  = ['1','3'];
+           setContentGutter(2, c);
+           var c  = ['1','3'];
+           setContentGutter(2, c);
+           var c  = ['1','3'];
+           setContentGutter(2, c);
+           var c  = ['1','3'];
+           setContentGutter(1, c);
+   }
