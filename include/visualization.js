333e1eaeda96921497a0fa4a0d68f80e56b7314c
alaboudi1 <alobodi1@hotmail.com>
2016-03-06 18:22:01 +0000

    Pastebin integration V1.0

--- include/visualization.js
+++ include/visualization.js
@@ -2,8 +2,12 @@
 
 $( document ).ready(function() {
   $("#note").hide();
+  
+    $('#popup').hide();
+    $('#hide').click(function(){
+        $('#popup').toggle();
 });
-
+});
 
 function toggle(checked){
   $("#note").toggle();
