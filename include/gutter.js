fb3aa4783ecba96d5aeaeed8c2f00561cde93db3
alaboudi1 <alobodi1@hotmail.com>
2016-03-07 00:56:47 +0000

    Fixes for the gutter

--- include/gutter.js
+++ include/gutter.js
@@ -4,13 +4,16 @@
    var index=  + "1";
 
    function  setContentGutter (line, content){
-        
+         var lastDiv = GetLastDiv();
          var lastline =jsEditor.getLastVisibleRow()+1;
          var iframeBody = $('#gutter');
 
          if(iframeBody.find('#line'+lastline).length ==0){
           CreateLine(lastline);
+          
          }
+         
+
         for(var i =0; i< content.length; i++){
         iframeBody.find("#line"+line).append(" [ "+content[i]+" ]");
         
@@ -22,14 +25,14 @@
    
    function CreateLine(line){
       var iframeBody = $('#gutter');  
-      var indexOfDiv = getLastDiv();
+      var indexOfDiv = GetLastDiv();
       for(indexOfDiv; indexOfDiv<=line; indexOfDiv++){
          iframeBody.append("<div id=line"+indexOfDiv+"></div>");
          iframeBody.find("#line"+indexOfDiv).addClass("line_height");
 
       }
-      
-      function getLastDiv(){
+   }
+      function GetLastDiv(){
       var iframeBody = $('#gutter');
        var indexOfDiv =1;   
           while (iframeBody.find('#line'+indexOfDiv).length !=0){
@@ -37,17 +40,30 @@
          }
        return indexOfDiv;
       }
-   }  
-      
+   
+      function RemoveLine(lastline, lastDiv){
+          var iframeBody = $('#gutter');  
+          while (lastline<lastDiv){
+            iframeBody.find('#line'+lastDiv).remove();
+              lastDiv--;
+         }
+      }
       
       function  highlightLine(){
       jsEditor.getSession().selection.on('changeCursor', function(e)
       {
+          var lastDiv = GetLastDiv();
           var iframeBody = $('#gutter');
           var line =  jsEditor.getCursorPosition().row+1;
-          if(iframeBody.find('#line'+line).length ==0){
-          CreateLine(line);
+          var lastline =jsEditor.session.getLength();
+
+          if(iframeBody.find('#line'+lastline).length ==0){
+                 CreateLine(lastline);
             }
+            if(lastline < lastDiv){
+                console.info(lastDiv)
+             RemoveLine(lastline, lastDiv);
+         }
           //console.info(selectedLine);
           //console.info("--->"+ window.index);
            iframeBody.find("#line"+selectedLine).removeClass("highlight_gutter");
@@ -60,14 +76,6 @@
       );
       }
    
-
-
-function scrolled(){
-    
-      jsEditor.getSession().setScrollTop(window.index);
-      
-     
-}
 
 
 
