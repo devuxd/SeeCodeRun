cd4e38540fe1a1bb2e293ee290f1dc93e243f990
Brandon Hilton <jbhilton11@gmail.com>
2016-03-10 01:08:08 +0000

    removed table vis folder and js and moved into vis-viewer.js since it was only calling one method

--- src/visViewer/vis-viewer.js
+++ src/visViewer/vis-viewer.js
@@ -21,12 +21,48 @@ export class VisViewer {
         for (let vis of this.visualizations) {
             switch(vis.type) {
                 case "table":
-                    let tableVis = new TableVis();
-                    tableVis.attached(vis.data);
+                    this.tabulate(vis.data);
                     break;
                 default:
                     break;
             }    
         }
     }
+    
+    // Method to create the table
+    tabulate(data) {
+            let columns = data.columns;
+            let values = data.values;
+            let table = d3.select(".visualization.table").append("table")
+                        .style("border-collapse", "collapse")// <= Add this line in
+                        .style("border", "2px black solid"),
+                thead = table.append("thead"),
+                tbody = table.append("tbody");
+        
+            // append the header row
+            thead.append("tr")
+                .selectAll("th")
+                .data(columns)
+                .enter()
+                .append("th")
+                    .text(function(column) { return column; });
+        
+            // create a row for each object in the data
+            let rows = tbody.selectAll("tr")
+                .data(values)
+                .enter()
+                .append("tr");
+        
+            // create a cell in each row for each column
+            let cells = rows.selectAll("td")
+                .data(function(row) {
+                    return columns.map(function(column) {
+                        return {column: column, value: row[column]};
+                    });
+                })
+                .enter()
+                .append("td")
+                .attr("style", "font-family: Courier") // sets the font style
+                    .html(function(d) { return d.value; });
+        }
 }
