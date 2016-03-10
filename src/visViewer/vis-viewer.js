599a8ae6b859061042f058db406d8da468adf271
Brandon Hilton <jbhilton11@gmail.com>
2016-03-10 01:05:49 +0000

    more working with the table visualization

--- src/visViewer/vis-viewer.js
+++ src/visViewer/vis-viewer.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import {TableVis} from '../tableVis/table-vis'
 
 export class VisViewer {
     
@@ -9,46 +10,23 @@ export class VisViewer {
     
     attached(visualizations) {
         this.visualizations = visualizations;
-        this.tabulate(this.visualizations);
+        this.generateVisualizations();
     }
     
     subscribe() {
       let ea = this.eventAggregator;
     }
     
-    tabulate(vis) {
-            let columns = vis[0].data.columns;
-            let values = vis[0].data.values;
-            let table = d3.select(".visualization.table").append("table")
-                        .style("border-collapse", "collapse")// <= Add this line in
-                        .style("border", "2px black solid"),
-                thead = table.append("thead"),
-                tbody = table.append("tbody");
-        
-            // append the header row
-            thead.append("tr")
-                .selectAll("th")
-                .data(columns)
-                .enter()
-                .append("th")
-                    .text(function(column) { return column; });
-        
-            // create a row for each object in the data
-            let rows = tbody.selectAll("tr")
-                .data(values)
-                .enter()
-                .append("tr");
-        
-            // create a cell in each row for each column
-            let cells = rows.selectAll("td")
-                .data(function(row) {
-                    return columns.map(function(column) {
-                        return {column: column, value: row[column]};
-                    });
-                })
-                .enter()
-                .append("td")
-                .attr("style", "font-family: Courier") // sets the font style
-                    .html(function(d) { return d.value; });
+    generateVisualizations() {
+        for (let vis of this.visualizations) {
+            switch(vis.type) {
+                case "table":
+                    let tableVis = new TableVis();
+                    tableVis.attached(vis.data);
+                    break;
+                default:
+                    break;
+            }    
         }
+    }
 }
