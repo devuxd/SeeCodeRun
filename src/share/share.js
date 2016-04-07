8a44029c353822ca8ff64eff874b2b484ac1de1f
alexmarcus87 <marcua2rpi@gmail.com>
2016-04-07 01:46:10 +0000

    steph alex share work 040616

--- src/share/share.js
+++ src/share/share.js
@@ -27,57 +27,66 @@ export class Share {
     //New Firebase share Reference
     var shareFirebaseRef = new Firebase(this.baseURL + '/' + this.pastebinId);
 
+    $(document).ready(function setUpShareBox() {
+          $('#shareDiv').hide();
+          
+          $('#share').click(function hideShareBox() {
+            $('#shareDiv').toggle();
+          });
+        
+        });
 
-document.getElementById("copyTarget").value = shareFirebaseRef;
 
-document.getElementById("copyButton").addEventListener("click", function() {
-    copyToClipboard(document.getElementById("copyTarget"));
-});
-
-function copyToClipboard(elem) {
-
-    var targetId = "_hiddenCopyText_";
-    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
-    var origSelectionStart, origSelectionEnd;
-    if (isInput) {
-        target = elem;
-        origSelectionStart = elem.selectionStart;
-        origSelectionEnd = elem.selectionEnd;
-    } else {
-        target = document.getElementById(targetId);
-        if (!target) {
-            var target = document.createElement("textarea");
-            target.style.position = "absolute";
-            target.style.left = "-9999px";
-            target.style.top = "0";
-            target.id = targetId;
-            document.body.appendChild(target);
+    document.getElementById("copyTarget").value = shareFirebaseRef;
+    
+    document.getElementById("copyButton").addEventListener("click", function() {
+        copyToClipboard(document.getElementById("copyTarget"));
+    });
+    
+    function copyToClipboard(elem) {
+    
+        var targetId = "_hiddenCopyText_";
+        var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+        var origSelectionStart, origSelectionEnd;
+        if (isInput) {
+            target = elem;
+            origSelectionStart = elem.selectionStart;
+            origSelectionEnd = elem.selectionEnd;
+        } else {
+            target = document.getElementById(targetId);
+            if (!target) {
+                var target = document.createElement("textarea");
+                target.style.position = "absolute";
+                target.style.left = "-9999px";
+                target.style.top = "0";
+                target.id = targetId;
+                document.body.appendChild(target);
+            }
+            target.textContent = elem.textContent;
         }
-        target.textContent = elem.textContent;
-    }
-    var currentFocus = document.activeElement;
-    target.focus();
-    target.setSelectionRange(0, target.value.length);
+        var currentFocus = document.activeElement;
+        target.focus();
+        target.setSelectionRange(0, target.value.length);
+        
+        var succeed;
+        try {
+        	  succeed = document.execCommand("copy");
+        } catch(e) {
+            succeed = false;
+        }
     
-    var succeed;
-    try {
-    	  succeed = document.execCommand("copy");
-    } catch(e) {
-        succeed = false;
-    }
-
-    if (currentFocus && typeof currentFocus.focus === "function") {
-        currentFocus.focus();
-    }
+        if (currentFocus && typeof currentFocus.focus === "function") {
+            currentFocus.focus();
+        }
+        
+        if (isInput) {
     
-    if (isInput) {
-
-        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
-    } else {
-
-        target.textContent = "";
+            elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+        } else {
+    
+            target.textContent = "";
+        }
+        return succeed;
     }
-    return succeed;
-}
-}
+  }
 }
