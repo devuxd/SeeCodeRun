134b2bd3231bdb4ee905265587cb90a075656512
alaboudi1 <alobodi1@hotmail.com>
2016-04-25 03:50:16 +0000

    working in selection and enhancing the data table

--- src/searchTab/trace-search.js
+++ src/searchTab/trace-search.js
@@ -2,14 +2,24 @@
 /* global Firebase */
 /* global ace */
 /* global $ */
-import {TraceModel} from '../traceService/trace-model';
-import {AceUtils} from '../utils/ace-utils';
+import {
+    TraceModel
+}
+from '../traceService/trace-model';
+import {
+    AceUtils
+}
+from '../utils/ace-utils';
 
-export class TraceSearch{
-    constructor(eventAggregator){
+export class TraceSearch {
+    constructor(eventAggregator) {
         this.eventAggregator = eventAggregator;
         this.traceModel = new TraceModel();
         this.aceUtils = new AceUtils();
+        this.options = [];
+        this.selectedFilter = 2;
+        this.heads=[];
+        this.rows = new Map();
         this.searchBox = {
             aceMarkerManager: undefined,
             updateAceMarkers: this.aceUtils.updateAceMarkers,
@@ -17,169 +27,193 @@ export class TraceSearch{
             $searchTerm: undefined,
             $searchFilter: undefined,
             $filteredOptions: undefined,
-            $table : undefined,
+            $table: undefined,
             markers: undefined,
             searchBoxChanged: undefined,
             updateTable: this.updateTable,
             searchFilters: this.traceModel.traceSearchfilters,
             traceHelper: undefined,
             $searchNumResults: undefined,
-            traceSearchEvents : this.traceModel.traceSearchEvents,
+            traceSearchEvents: this.traceModel.traceSearchEvents,
             eventAggregator: eventAggregator,
             publishTraceSearchChanged: this.publishTraceSearchChanged,
             publishAceMarkersChanged: this.publishAceMarkersChanged
         };
     }
 
-    attached(aceEditor, $table = $("#traceTable")){
+    attached(aceEditor, $table = $("#traceTable")) {
         let searchBox = this.searchBox;
-        
         searchBox.aceMarkerManager = this.aceUtils.makeAceMarkerManager(aceEditor);
         searchBox.$table = $table;
         searchBox.$searchTerm = $("#searchTerm");
         searchBox.$searchFilter = $("#searchFilter");
         searchBox.$filteredOptions = $("#filteredOptions");
         searchBox.$searchNumResults = $("#resultCount");
-        
-        searchBox.$searchFilter.empty();
-        let options ="";
-        for (let id in searchBox.searchFilters){
-            let value = searchBox.searchFilters[id];
-            options += `<option value = "${id}">${value}</option>`;
+
+         this.searchBox = searchBox;  
+        // searchBox.$searchFilter.empty();
+
+        for (let filter in searchBox.searchFilters) {
+            // let value = searchBox.searchFilters[id];
+            // options += `<option value = "${id}">${value}</option>`;
+
+            // console.info(filter);
+            this.options.push(filter);
+
         }
-        searchBox.$searchFilter.append(options);
-        
-        let searchBoxChanged = function searchBoxChanged(e){
-            let selectedFilter = searchBox.$searchFilter.find(":selected").val();
-            let value = searchBox.$searchTerm.val();
-            
-            searchBox.publishTraceSearchChanged(searchBox, value, selectedFilter);
-        };
-        searchBox.searchBoxChanged = searchBoxChanged;
-        searchBox.$searchTerm.change(searchBoxChanged);
-        searchBox.$searchFilter.change(searchBoxChanged);
-        
-        this.subscribe(); 
-        
-                
-    
+
+       
+        this.subscribe();
+
+
+
     }
-    
-    publishTraceSearchChanged(searchBox, searchTermText, searchFilterId){
-        searchBox.eventAggregator.publish(searchBox.traceSearchEvents.searchBoxChanged.event, 
-            { searchTermText: searchTermText, searchFilterId: searchFilterId }
-        );
-    }
-    publishAceMarkersChanged(searchBox, itemsWithRanges){
-        searchBox.eventAggregator.publish(searchBox.traceSearchEvents.aceMarkersChanged.event,
-            {items: itemsWithRanges}
-        );
-    }
-    
-    subscribe(){
-        let searchBox = this.searchBox;
-        let traceChangedEvent = this.traceModel.traceEvents.changed.event;
-        let searchBoxChangedEvent = this.traceModel.traceSearchEvents.searchBoxChanged.event;
-        let aceMarkersChangedEvent = this.traceModel.traceSearchEvents.aceMarkersChanged.event;
-        
-        this.eventAggregator.subscribe( traceChangedEvent, payload =>{
-            searchBox.traceHelper = payload.data;
-            searchBox.searchBoxChanged(traceChangedEvent);
-            
+
+    publishTraceSearchChanged(searchTermText, searchFilterId) {
+        this.eventAggregator.publish('searchBoxChanged', {
+            searchTermText: searchTermText,
+            searchFilterId: searchFilterId
         });
-        
-        this.eventAggregator.subscribe( searchBoxChangedEvent, payload =>{
+    }
+    publishAceMarkersChanged( itemsWithRanges) {
+        this.eventAggregator.publish('aceMarkersChanged', {
+            items: itemsWithRanges
+        });
+    }
+
+    subscribe() {
+        let searchBox = this.searchBox;
+       
+        this.eventAggregator.subscribe('traceChanged', payload => {
+            searchBox.traceHelper = payload.data;
+        //   searchBox.searchBoxChanged('traceChanged');
+        });
+
+        this.eventAggregator.subscribe('searchBoxChanged', payload => {
             let value = payload.searchTermText;
             let selectedFilter = payload.searchFilterId;
-                //alert(value);
-                
-                if(searchBox.traceHelper){
-                let variableValues =searchBox.traceHelper.getValues();
-                let query = searchBox.traceHelper.traceQueryManager.getQuery(variableValues, selectedFilter, value);
-                searchBox.updateTable(searchBox, query);
-                
+            //alert(value);
+
+            if (searchBox.traceHelper) {
+                let variableValues = searchBox.traceHelper.getValues();
+              //update ta
+               let query = searchBox.traceHelper.traceQueryManager.getQuery(variableValues, selectedFilter, value);
+            //   console.info(query.item[0]);
+               this.updateTable(searchBox,query);
             }
         });
-        
+
         let updateAceMarkersTimeout;
         let updateAceMarkersSetTimeout = window.setTimeout;
         let updateAceMarkersClearTimeout = window.clearTimeout;
-        
-        this.eventAggregator.subscribe( aceMarkersChangedEvent, payload =>{
+
+        this.eventAggregator.subscribe('aceMarkersChanged', payload => {
             let items = payload.items;
             updateAceMarkersClearTimeout(updateAceMarkersTimeout);
             updateAceMarkersTimeout = updateAceMarkersSetTimeout(
                 searchBox.updateAceMarkers(searchBox.aceMarkerManager, items),
                 searchBox.updateAceMarkersDelay);
         });
-        
+
     }
-    
-    updateTable(searchBox, query){
-        
-        let $table = searchBox.$table;
-        let $numResults = searchBox.$searchNumResults;
-        let selectedFilter = searchBox.$searchFilter.find(":selected").val();
-        
-        if(!$table){
-            throw "No table container received.";
-        }
 
-        if(!query){
-            throw "No query received.";
-        }
-        
-        if(query.count()<1){
-            $table.innerHTML = "No Results Found. Try a different Search Term with Filter Option.";
-            return;
-        }
-        let table = "<table>";
+    updateTable(searchBox, query) {
 
-        let header ="<tr>";
-        let columns = [];
-        for(let key in query.items[0]){
-            if(key !== "range"){
-                header += `<td>${key}</td>`;
-                columns[key] = true;
-            }
-        }
-        header += "</tr>";
-        
-        table += header;
-        
-        let dataList = [];
-        for(let i = 0; i < query.items.length; i++){
+        //     let $table = searchBox.$table;
+        //     let $numResults = searchBox.$searchNumResults;
+        // let selectedFilter = this.selectedFilter;
 
-            let row = "<tr>";
-            for(let key in columns){
-                //TODO add tooltip and new css when selected
-                let value = query.items[i][key];
-                row += `<td>${value}</td>`;
-                
-                if(selectedFilter === "any" || key === selectedFilter){
-                   dataList[value]++;
+        //     if(!$table){
+        //         throw "No table container received.";
+        //     }
+
+        //     if(!query){
+        //         throw "No query received.";
+        //     }
+
+        //     if(query.count()<1){
+        //         $table.innerHTML = "No Results Found. Try a different Search Term with Filter Option.";
+        //         return;
+        //     }
+        //     let table = "<table>";
+
+            // let header ="<tr>";
+            
+            this.heads=[];
+            for(let key in query.items[0]){
+                if(key !== "range"){
+                   
+                   
+                    this.heads.push(key);
+                    // header += `<td>${key}</td>`;
+                    // columns[key] = true;
                 }
             }
-            row += "</tr>";
-            
-            table+= row;
-         }
-        
-        table += "</table>";
+            // console.info(this.heads);
+        //     header += "</tr>";
 
-        searchBox.publishAceMarkersChanged(searchBox, query.items);
+        //     table += header;
+
+        //     let dataList = [];
+          
+
+            for(let i = 0; i < query.items.length; i++){
+                let contents ={};
+                for(let key of this.heads){
+        //             //TODO add tooltip and new css when selected
+        //             let value = query.items[i][key];
+        //             row += `<td>${value}</td>`;
+                    contents[key] =query.items[i][key];
+        //             if(selectedFilter === "any" || key === selectedFilter){
+        //               dataList[value]++;
+        //             }
+        //         }
+        //         row += "</tr>";
+
+        //         table+= row;
+             }
+             console.info("Hello");
+                this.rows.set(i,contents);
+
+            }
+        //     table += "</table>";
+
+        //   searchBox.publishAceMarkersChanged(searchBox, query.items);
+
+        //     searchBox.$filteredOptions.empty();
+        //     let options ="";
+        //       for(let value  of this.rows.values()){
+        //           options += `<option>${value}</option>`;
+                  
+        //           console.info(value);
+            //   }
+        //     searchBox.$filteredOptions.append(options);
+
+        //     $table.html(table);   
+        //     $numResults.html(`<p>Number of Search Results: ${query.count()}</p>`);
+    
         
-        searchBox.$filteredOptions.empty();
-        let options ="";
-        for(let value in dataList){
-             options += `<option>${value}</option>`;
-        }
-        searchBox.$filteredOptions.append(options);
-        
-        $table.html(table);   
-        $numResults.html(`<p>Number of Search Results: ${query.count()}</p>`);
+        this.numberOfResult=this.rows.size;
     }
+                // console.info(this.rows);
+
     
-    
-  
+
+    filterChanged() {
+        this.selectedFilter= this.selectedFilter;
+        // console.info(this.selectedFilter);
+        let searchBox = this.searchBox;
+         let selectedFilter = this.selectedFilter
+        // console.info(`First ${selectedFilter}`)
+       
+
+            let value = searchBox.$searchTerm.val();
+            // console.info(`Second  ${selectedFilter}`)
+
+            searchBox.publishTraceSearchChanged(value, selectedFilter);
+        
+       
+
+    }
+
 }
