376d399802ac79e3912c20948d669b501c50e7d5
David Gonzalez <luminaxster@gmail.com>
2016-07-06 04:35:30 +0000

    unused files removed

--- include/pure-js-pre-loader.js
+++
@@ -1,9 +0,0 @@
-/* global Split */
-
-var windowOnload = window.onload;
-document.onload = function(){
-    if(windowOnload){
-        windowOnload();
-    }
-    // pre-aurelia load code goes here:
-};
