939afae6d3e01fcbb44a7c816d2e2f2ed07d9159
David Gonzalez <luminaxster@gmail.com>
2017-07-11 13:28:05 -0400

    Added beta events and logic for showing the navigator in the callgraph nodes

--- src/visualAnalysis/graphical-analyzer.js
+++ src/visualAnalysis/graphical-analyzer.js
@@ -1,6 +1,7 @@
 /**
  * Created by DavidIgnacio on 7/10/2017.
  */
+import {AceUtils} from '../utils/ace-utils';
 export class GraphicalAnalyzer{
   attribute = "h";
 
@@ -14,10 +15,22 @@ export class GraphicalAnalyzer{
   }
 
   subscribe(){
+    let aceUtils = new AceUtils();
+    let aceEditor = ace.edit('aceJsEditorDiv'); // example
+    let aceMarkerManager = aceUtils.makeAceMarkerManager(aceEditor, aceUtils.getAvailableMarkers().errorMarker, "line", true);
       this.eventAggregator.subscribe("traceChanged", payload=>{
         let traceHelper = payload.data;
         //todo: David will create getGraphicalObjectsFromTimeline() with the references to DOM elements
-        console.log("GA", traceHelper.getTimeline());
+        let timeline = traceHelper.getTimeline();
+        // console.log("GA", );
+        let identifiers = [];
+        for (let index in timeline) {
+          //console.log("", timeline[index]);
+          if (timeline[index].type === "VariableDeclarator") {
+            identifiers.push(timeline[index]);
+          }
+        }
+        aceUtils.updateAceMarkers(aceMarkerManager, identifiers);
       });
   }
 }
