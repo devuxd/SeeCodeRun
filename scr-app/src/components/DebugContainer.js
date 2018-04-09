38c3764570183b84598aab5e7e0d8f5bca8eacf9
luminaxster <84goLiat_Git>
2018-04-09 09:40:34 -0400

    removes scr-app/build from git

--- scr-app/src/components/DebugContainer.js
+++ scr-app/src/components/DebugContainer.js
@@ -1,81 +1,78 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {withStyles} from 'material-ui/styles';
+import {withStyles, AppBar, Typography, Tabs, Tab} from 'material-ui';
 import SwipeableViews from 'react-swipeable-views';
-import AppBar from 'material-ui/AppBar';
-import Tabs, {Tab} from 'material-ui/Tabs';
-import Typography from 'material-ui/Typography';
 
 import TraceTable from './TraceTable';
 import TraceToolbar from './TraceToolbar';
 
 function TabContainer({children, dir}) {
-  return (
-    <Typography component="div" dir={dir}>
-      {children}
-    </Typography>
-  );
+    return (
+        <Typography component="div" dir={dir}>
+            {children}
+        </Typography>
+    );
 }
 
 TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-  dir: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+    dir: PropTypes.string.isRequired,
 };
 
 const styles = theme => ({
-  root: {
-    backgroundColor: theme.palette.background.paper,
-  },
+    root: {
+        backgroundColor: theme.palette.background.paper,
+    },
 });
 
 class DebugContainer extends React.Component {
 
-  render() {
-    const {classes, appClasses, theme, tabIndex, handleChangeTab, handleChangeTabIndex} = this.props;
+    render() {
+        const {classes, appClasses, theme, tabIndex, handleChangeTab, handleChangeTabIndex} = this.props;
 
-    return (
-      <div style={{minWidth:500}}>
-        <AppBar position="sticky" color="default">
-          <TraceToolbar/>
-          <Tabs
-            value={tabIndex}
-            onChange={handleChangeTab}
-            indicatorColor="primary"
-            textColor="primary"
-            fullWidth
-            centered
-          >
-            <Tab label="Trace"/>
-            <Tab label="Streams"/>
-            {/*<Tab label="Visualizations"/>*/}
-          </Tabs>
-        </AppBar>
+        return (
+            <div style={{minWidth: 500}}>
+                <AppBar position="sticky" color="default">
+                    <TraceToolbar/>
+                    <Tabs
+                        value={tabIndex}
+                        onChange={handleChangeTab}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        fullWidth
+                        centered
+                    >
+                        <Tab label="Trace"/>
+                        <Tab label="Streams"/>
+                        {/*<Tab label="Visualizations"/>*/}
+                    </Tabs>
+                </AppBar>
 
-        <SwipeableViews
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-          index={tabIndex}
-          onChangeIndex={handleChangeTabIndex}
-        >
-          <TabContainer dir={theme.direction} >
-            <TraceTable/>
-          </TabContainer>
-          <TabContainer dir={theme.direction} >
-            Code edit history and dumped traces
-          </TabContainer>
-          {/*<TabContainer dir={theme.direction}>*/}
+                <SwipeableViews
+                    axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                    index={tabIndex}
+                    onChangeIndex={handleChangeTabIndex}
+                >
+                    <TabContainer dir={theme.direction}>
+                        <TraceTable/>
+                    </TabContainer>
+                    <TabContainer dir={theme.direction}>
+                        Code edit history and dumped traces
+                    </TabContainer>
+                    {/*<TabContainer dir={theme.direction}>*/}
 
-          {/*D3 soon...*/}
+                    {/*D3 soon...*/}
 
-          {/*</TabContainer>*/}
-        </SwipeableViews>
-      </div>
-    );
-  }
+                    {/*</TabContainer>*/}
+                </SwipeableViews>
+            </div>
+        );
+    }
 }
 
 DebugContainer.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles, {withTheme: true})(DebugContainer);
