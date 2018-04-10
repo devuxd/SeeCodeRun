4b2ec9f4883d148cd69b793a11c30f80761409fe
luminaxster <84goLiat_Git>
2018-04-09 21:07:08 -0400

    Fixes build minification (except jscodeshift, chunk is left as is). updates all dependencies

--- scr-app/config-overrides.js
+++ scr-app/config-overrides.js
@@ -1,6 +1,6 @@
-//source: https://github.com/jdcrensh/create-react-app/blob/jdcrensh/packages/react-scripts-plugin-no-minify/utils.js
-const filterPlugins = (config, filter) =>
-    config.plugins.filter(p => filter[getFunctionName(p)] !== false);
+// based on: https://github.com/jdcrensh/create-react-app/blob/jdcrensh/packages/react-scripts-plugin-no-minify/utils.js
+const findPlugin = (config, pluginName) =>
+    config.plugins.find(p => getFunctionName(p) === pluginName);
 
 const getFunctionName = obj => {
     const funcNameRegex = /(function (.{1,})\(|class (.{1,}))/;
@@ -8,58 +8,12 @@ const getFunctionName = obj => {
     return results && results.length > 1 ? /\w+ (\w+)/.exec(results[1])[1] : '';
 };
 
-module.exports = (config/*, env*/) => { // todo fix jscodeshift breaking minification phase
-    config.plugins = filterPlugins(config, {
-        UglifyJsPlugin: false,
-    });
-
+module.exports = (config/*, env*/) => { // todo jscodeshift breaking minification phase (fix: excluding it)
+    const plugin = findPlugin(config, 'UglifyJsPlugin');
+    if (plugin) {
+        //chunks: [firepad, firebase, jscodeshift, babel]
+        // so far jscodeshift is chunck 2, it can change with more code splits
+        plugin.options.exclude = [/\/js\/2\..*chunk.js$/];
+    }
     return config;
 };
-
-// const findPlugin = (config, pluginName) =>
-//     config.plugins.find(p => getFunctionName(p) === pluginName);
-//
-// const plugin = findPlugin(config, 'UglifyJsPlugin');
-// if (plugin) {
-//     // const i = config.plugins.indexOf(plugin);
-//     //   const webpack = require('webpack');
-//     plugin.exclude = /\/jscodeshift/;
-//     plugin.options.paralell = true;
-//     plugin.options.warnings = true;
-//     // config.plugins[i] = new webpack.optimize.UglifyJsPlugin({
-//     //   //  exclude: /\/jscodeshift/
-//     //     // ,
-//     //     uglifyOptions: plugin.options
-//     // });
-//     plugin.uglifyOptions= plugin.options;
-//     // delete plugin.options;
-//
-//     // plugin.options.exclude= /\/jscodeshift/;
-//     console.log("R", plugin);//, config.plugins
-// }
-
-//console.log('RULES', config.plugins);
-//config.externals=
-// config.module.rules[0].exclude =  /\/jscodeshift/;
-//console.log('PLUGINS', config.plugins);
-
-
-
-// //const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-// const findPlugin = (config, pluginName) =>
-//     config.plugins.find(p => getFunctionName(p) ===pluginName);
-// const plugin = findPlugin(config, 'UglifyJsPlugin');
-// if(plugin){
-//     // const i = config.plugins.indexOf(plugin);
-//     // config.plugins[i] = new UglifyJsPlugin({
-//     //     exclude: /\/jscodeshift/
-//     // });
-//     plugin.options.paralell= true;
-//     plugin.options.warnings= true;
-//    // plugin.options.exclude= /\/jscodeshift/;
-//     console.log("R",  plugin);//, config.plugins
-// }
-
-// "start": "react-scripts start",
-//     "build": "react-scripts build",
-//     "test": "react-scripts test --env=jsdom",
