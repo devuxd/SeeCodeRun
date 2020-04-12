9aa7755ff7c0245168223a884390346c6400ce3c
David Samudio <luminaxster@gmail.com>
2020-04-12 19:28:35 -0400

    updates google fonts to ccs2.

--- scr-app/src/common/pastebinContent.js
+++ scr-app/src/common/pastebinContent.js
@@ -67,8 +67,7 @@ ReactDOM.render(<App />, document.querySelector("#root"));
 const htmlDefaultText = `<!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
 </head>
 <body>
 	<div id="root"></div>
